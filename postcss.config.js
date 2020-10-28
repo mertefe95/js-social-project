d28bd25d0bab8d77a31d9b534868f2467265f7f7
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-10-29 01:25:32 +0300

    last time

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,20 @@
 module.exports = {
-    plugins: [
-      "postcss-import",
-      "tailwindcss",
-      "autoprefixer"
-    ]
-  };
-  
+  plugins: [
+    'tailwindcss',
+    process.env.NODE_ENV === 'production'
+      ? [
+          '@fullhuman/postcss-purgecss',
+          {
+            content: [
+              './src/pages/**/*.{js,jsx,ts,tsx}',
+              './src/components/**/*.{js,jsx,ts,tsx}',
+              './src/components/**/**/*.{js,jsx,ts,tsx}',
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        ]
+      : undefined,
+    'postcss-preset-env',
+  ],
+};
